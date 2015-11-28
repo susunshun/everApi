b3b89d9d7cf51843bfcc0c62c0986bceae731988
susunshun <your.email@example.com>
2015-11-28 21:38:54 +0900

    20151128

--- everApi.rb
+++ everApi.rb
@@ -9,6 +9,11 @@ get '/' do
         "<h1>Wooooooooooooo!<h1>"
 end
 
+
+get '/test' do
+    "fuck you"
+end
+
 get '/random' do
     #デベロッパートークン
     auth_token = "S=s1:U=91b5c:E=15877480ee1:C=1511f96df40:P=1cd:A=en-devtoken:V=2:H=dd73cdc1d04ff06afc122c887b0d81ed"
